31ffe33115f11dc5f8b8bf7007dca7f1ca888707
Samuel Adam <samuel@agilizelabs.com>
2014-09-09 23:05:31 +0545

    add popup on homepage

--- js/pdp/js/frontend_popup.js
+++ js/pdp/js/frontend_popup.js
@@ -1,6 +1,6 @@
 var frontendPopup = jQuery.noConflict();
 frontendPopup(function($) {
-    var baseUrl = $("#base_url").val(),
+    var baseUrl = $("#base_url").val() || BASE_URL,
             currentProductId = $("#current_product_id").val(),
             designPageUrl = baseUrl + "pdp/view/getDesignPage",
             w_window = $(window).width();
@@ -18,6 +18,20 @@ frontendPopup(function($) {
                     //$('#pdp_design_button').click();
                 }
             });
+
+            $(".homeitem .startnow").on("click", function() {
+                if (!$(this).hasClass("loaded")) {
+                    
+                    var cartItemId = $("#cart_item_id").val() || '',
+                            shareId = $("#pdp_design_share").val() || '',
+                            currentProductId = 890,
+                            pdpData = {product_id: currentProductId, item_id: cartItemId, share_id: shareId};
+                    LoadDesign.sendRequest(designPageUrl, pdpData, LoadDesign.prepareDesignPage);
+                } else {
+                    //show popup here
+                    //$('#pdp_design_button').click();
+                }
+            });
         }(),
         init: function() {
             var action = $("#pdp_design_action").val(),
@@ -26,10 +40,13 @@ frontendPopup(function($) {
             if (action != "" || shareId != "" || cartItemId != "") {
                 //Validate product form
                 var addToCartForm = new VarienForm('product_addtocart_form', true);
-                if (addToCartForm.validator.validate()) {
-                    $(window).load(function() {
-                        $(".design-btn .design-product").click();
-                    });
+                if (addToCartForm.form != null) {
+
+                    if (addToCartForm.validator.validate()) {
+                        $(window).load(function() {
+                            $(".design-btn .design-product").click();
+                        });
+                    }
                 }
             }
         }(),
@@ -49,7 +66,7 @@ frontendPopup(function($) {
             });
         },
         prepareDesignPage: function(data) {
-            $(".design-btn .design-product").addClass('loaded');
+            //$(".design-btn .design-product").addClass('loaded');
             $('body').append('<div id="pdp_popup"><div class="overlay"></div></div>');
             $("#pdp_design_popup").appendTo($('#pdp_popup')).html(data);
             var w_design = $('.wrap_inlay_center').width(),
