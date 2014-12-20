c41a9837ee03ddaabf3962d5c0b330b8e378e7cd
Samuel Adam <samuel@agilizelabs.com>
2014-12-20 22:38:06 +0545

    debug: responsive canvase

--- js/pdp/js/design.js
+++ js/pdp/js/design.js
@@ -1520,16 +1520,14 @@
                     var canvas_export = new fabric.Canvas('canvas_export', {
                         opacity: 1
                     });
-                    console.log('test' + backgroundImg);
+                    //console.log('test' + backgroundImg);
                     // set background for export image  
-                    canvas_export.setBackgroundImage(backgroundImg, canvas_export.renderAll.bind(canvas_export), {
-                        'scaleX': (1 / 0.7),
-                        'scaleY': (1 / 0.7),
-                        'originX': 'left',
-                        'originY': 'top',
-                        'left': 0,
-                        'top': 0
-                    });
+//                    canvas_export.setBackgroundImage(backgroundImg, canvas_export.renderAll.bind(canvas_export), {
+//                        'originX': 'left',
+//                        'originY': 'top',
+//                        'left': 0,
+//                        'top': 0
+//                    });
                     // Add added image from another canvas
                     fabric.Image.fromURL(canvas.toDataURL('png'), function (image) {
                         image.set({
