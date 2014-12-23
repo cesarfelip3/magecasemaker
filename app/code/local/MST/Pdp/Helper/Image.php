35a445c9f4a91c6e59f5471bb8ccf48ae95a4f74
hello <hello@hellodeMacBook-Air.local>
2014-12-23 11:07:40 +0800

    update

--- app/code/local/MST/Pdp/Helper/Image.php
+++ app/code/local/MST/Pdp/Helper/Image.php
@@ -86,6 +86,22 @@ class MST_Pdp_Helper_Image extends Mage_Core_Helper_Abstract
             "bottom" => 1894,
             "width" => 1800,
             "height" => 2492
+        ),
+        "iphone6plus" => array(
+            "left" => 640,
+            "top" => 434,
+            "right" => 0,
+            "bottom" => 0,
+            "width" => 2340,
+            "height" => 2340
+        ),
+        "iphone6" => array(
+            "left" => 640,
+            "top" => 434,
+            "right" => 1685,
+            "bottom" => 1894,
+            "width" => 1800,
+            "height" => 2492
         )
     );
 
